c6142d54058fa584c942348751d4215764834faa
Pedro Simeão Carvalho <pesimeao@users.noreply.github.com>
2015-09-18 09:31:06 +0100

    Update BackgroundBeaconService.java

--- src/android/BackgroundBeaconService.java
+++ src/android/BackgroundBeaconService.java
@@ -19,57 +19,69 @@ import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
 import org.altbeacon.beacon.startup.BootstrapNotifier;
 import org.altbeacon.beacon.startup.RegionBootstrap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BackgroundBeaconService extends Service implements BootstrapNotifier {
 
     public BackgroundBeaconService() {
         super();
     }
 
-    private BackgroundPowerSaver backgroundPowerSaver;
     private BeaconManager iBeaconManager;
-    private RegionBootstrap regionBootstrap;
+
     @Override
     public void onCreate() {
-        Log.d("customer-tlantic", "BACKGROUND: Creating BackgroundBeaconService.");
-
         super.onCreate();
-        iBeaconManager = BeaconManager.getInstanceForApplication(this);
-        iBeaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
 
-        iBeaconManager.setBackgroundBetweenScanPeriod(1000l);
-        iBeaconManager.setBackgroundScanPeriod(5000l);
-        // Simply constructing this class and holding a reference to it
-        // enables auto battery saving of about 60%
-        //backgroundPowerSaver = new BackgroundPowerSaver(this);
+        Log.d("CustomerTlantic", "BACKGROUND: Creating BackgroundBeaconService.");
+
+        // Start beacons service
+        iBeaconManager = BeaconManager.getInstanceForApplication(this);
+
+        // Settings
+        iBeaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        iBeaconManager.setBackgroundBetweenScanPeriod(1000L);
+        iBeaconManager.setBackgroundScanPeriod(5000L);
         iBeaconManager.setDebug(true);
 
+        // Simply constructing this class and holding a reference to it
+        // enables auto battery saving of about 60%
+        // backgroundPowerSaver = new BackgroundPowerSaver(this);
 
-        Region region = new Region("backgroundRegion",
-                Identifier.parse("b9407f30-f5f8-466e-aff9-25556b57fe6d"), Identifier.parse("32928"), Identifier.parse("441"));
-        regionBootstrap = new RegionBootstrap(this, region);
-        Log.d("customer-tlantic", "BACKGROUND: Created RegionBootstrap in BackgroundBeaconService.");
+        // Create regions
+        List<Region> regions = new ArrayList<Region>();
+
+        regions.add(new Region("backgroundRegion", Identifier.parse("b9407f30-f5f8-466e-aff9-25556b57fe6d"), Identifier.parse("43549"), Identifier.parse("64185")));
+
+        // Register regions to watch
+        RegionBootstrap regionBootstrap = new RegionBootstrap(this, regions);
+
+        Log.d("CustomerTlantic", "BACKGROUND: Created RegionBootstrap in BackgroundBeaconService.");
     }
+
     @Override
-    public void onDestroy(){
-        Log.d("customer-tlantic", "Destroying BackgroundBeaconService");
+    public void onDestroy() {
+        Log.d("CustomerTlantic", "Destroying BackgroundBeaconService");
     }
 
     @Override
     public void didEnterRegion(Region region) {
-        Log.d("customer-tlantic", "BackgroundBeaconService.didEnterRegion called!");
+        Log.d("CustomerTlantic", "BackgroundBeaconService.didEnterRegion called!");
+
         Intent intent = new Intent();
-        intent.setAction("com.example.andrepinto.NOTIFY_FOR_BEACON");
+        intent.setAction("com.tlantic.customer.demo.NOTIFY_FOR_BEACON");
         getApplicationContext().sendOrderedBroadcast(intent, null);
     }
 
     @Override
     public void didExitRegion(Region region) {
-        Log.d("customer-tlantic", "BackgroundBeaconService.didExitRegion called!");
+        Log.d("CustomerTlantic", "BackgroundBeaconService.didExitRegion called!");
     }
 
     @Override
     public void didDetermineStateForRegion(int i, Region region) {
-        Log.d("customer-tlantic", "BackgroundBeaconService.didDetermineStateForRegion called!");
+        Log.d("CustomerTlantic", "BackgroundBeaconService.didDetermineStateForRegion called!");
     }
 
     @Override
